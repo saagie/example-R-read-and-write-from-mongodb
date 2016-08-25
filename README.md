9ce300ff700b28c649ea34994f19f6d87ed56448
Tony <tony@creativedata.fr>
2016-08-25 18:20:13 +0200

    feat: read and write with security

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# example-R-read-and-write-from-mongodb
-Example for Saagie Wiki - Read and write to MongoDB with R
+Read and write data on MongoDB from R
+==================================
+
+## Dependencies
+- R package `mongolite` -> Used to interact with MongoDB.
+- Linux package `libssl-dev` and `libsasl2-dev` -> Dependencies of the `mongolite` package. Can be installed with the following command `sudo apt-get install -y libssl-dev libsasl2-dev`.
+
+The commands to install theses dependencies are also presents in the source files.
+
+
+## Usage
+
+- Fill the connection parameters
+- Source the file you want to execute.
---
+++ example-with-security.R
@@ -0,0 +1,40 @@
+# Choose a cran mirror to use (48 = Paris / France)
+chooseCRANmirror(ind = 48)
+
+# Install dependencies if necessary
+if (!require(mongolite)) {
+    # Install system dependencies
+    system("sudo apt-get install -y libssl-dev libsasl2-dev")
+    install.packages("mongolite")
+    library(mongolite)
+}
+
+### Parameters to set
+# Specials characters must be encoded as url, see here : http://www.w3schools.com/tags/ref_urlencode.asp
+user <- "user"
+password <- "password"
+ip <- "xxx.xxx.xxx.xxx"
+port <- "27017"
+authentificationDatabase <- "admin"
+database <- "database"
+collection <- "MyNewCollection"
+data <- mtcars
+
+# Build the connection uri from the parameters above
+uri <- paste0("mongodb://", user, ":", password, "@", ip, ":", port, "/", authentificationDatabase)
+
+# Connect to the database
+con <- mongo(collection = collection, db = database, url = uri)
+
+# Count the number of documents in the collection
+con$count()
+
+# Insert new data in the collection
+con$insert(data)
+
+# Read data from the collection
+con$find()
+
+# Read data from any valid mongo query
+con$find('{"cyl" : 6, "mpg" : { "$gt" : 20}}')
+
